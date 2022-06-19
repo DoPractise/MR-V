6d2ba152f4efa4d0ff3a67a1280cc203f4d2a1f8
Physeter <116091iyh@gmail.com>
2022-06-19 23:50:13 +0800

    添加合成pdf例子

---
+++ src/views/pdf/compose.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>合成pdf
+https://juejin.cn/post/7042515283419856910
+https://gitee.com/yuanfanwo/pdf-sign
+  </div>
+</template>
+<script>
+export default {
+  name: "Compose",
+  data() {
+    return {};
+  },
+  methods: {
+
+  }
+};
+</script>
